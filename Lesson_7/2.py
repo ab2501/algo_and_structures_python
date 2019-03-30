6ad5acc9b94c0bb34cbac41e0d83a9a64cc85475
ab2501 <rorsha@gmail.com>
2019-03-30 11:23:12 +0300

    DZ lesson 7 a.butarev

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,35 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+
+import random
+array = [round(random.uniform(0.0, 50.0), 2) for i in range(0, 10)]
+
+
+def sorted_by_merge(array: list):
+
+    def split_array(array: list):
+        if len(array) <= 1:
+            return array
+
+        left = []
+        right = []
+        rand_index = random.randint(0, len(array) - 1)
+        rand_element = array[rand_index]
+        array.pop(rand_index)
+
+        for item in array:
+            if item >= rand_element:
+                right.append(item)
+            else:
+                left.append(item)
+
+        # print(f'--> {left} {[rand_element]} {right}')
+        return split_array(left) + [rand_element] + split_array(right)
+    return split_array(array)
+
+
+
+print(f' Исходный массив:{array}')
+array = sorted_by_merge(array)
+print(f' Отсортированный массив:{array}')
--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,33 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+import random
+
+M = 5
+
+array = [random.randint(0, 100) for _ in range(2 * M + 1)]
+print(f'дан массив: {array}')
+# print(sorted(array), sorted(array)[M])
+
+
+count = [[0, 0] for i in range(len(array))]
+max_ = 0
+for key_i, val_i in enumerate(array):
+    for val_j in array:
+        if val_j < val_i:
+            count[key_i][0] += 1
+        elif val_j > val_i:
+            count[key_i][1] += 1
+
+    if val_i > max_:
+        max_ = val_i
+
+res = 0
+min_ = max_
+for key, val in enumerate(count):
+    med = abs(val[1] - val[0])
+    if med < min_:
+        min_ = med
+        res = array[key]
+print(f'его медиана равна {res}')
